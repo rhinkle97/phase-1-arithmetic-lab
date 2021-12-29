d936ad5c2fac26841ed08401bc8b8a76a53d2b50
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-12-29 12:55:47 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
 
+let random = Math.ceil(Math.random());
+
+const num3 = 4
+const num4 = 16
+let mod = (num3 % num4);
+
+let max = Math.max(1,5,10,15,20);
 // Write your code here
