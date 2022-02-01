9d8b34d35f11893621e01f515095a1f7eca4d0e8
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-01 05:40:52 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// multiplied results equal 62
+// generates randomn integer > 0
+// equation that calculates remainder that is equal to 4
+// will return 20 as highest number in the set
+
 const num1 = 31;
 const num2 = 2;
-let multiply = (num1 * num2);
+const multiply = num1 * num2;
 
-let random = Math.ceil(Math.random());
+//const random = Math.random();
+const random = Math.floor(Math.random() * 10) + 1;
 
-const num3 = 4
-const num4 = 16
-let mod = (num3 % num4);
+const num3 = 31;
+const num4 = 9;
+const mod = num3 % num4;
 
-let max = Math.max(1,5,10,15,20);
-// Write your code here
+
+const max = Math.max(1,5,10,20);
